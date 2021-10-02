4a289ac88bcb32c40bd915a7bf0e201235e064e1
Navya32 <navyagupta32@gmail.com>
2021-10-02 23:13:37 +0530

    video play button added

--- vid.js
+++ vid.js
@@ -1,7 +1,30 @@
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
+// MODAL
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
-})
+$(document).ready(function () {
+  // Gets the video src from the data-src on each button
+
+  var $videoSrc;
+  $('.video-btn').click(function () {
+    $videoSrc = $(this).data('src');
+  });
+
+  // when the modal is opened autoplay it
+  $('#myModal').on('shown.bs.modal', function (e) {
+
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $('#video').attr('src', $videoSrc + '?autoplay=1&amp;modestbranding=1&amp;showinfo=0');
+  });
+
+  // stop playing the youtube video when I close the modal
+  // $('#myModal').on('hide.bs.modal', function (e) {
+  //     // a poor man's stop video
+  //     // $("#myModal iframe").attr('src', $("myModal iframe").attr("src"));
+  //     $("#video").attr('src',$videoSrc);
+  // });
+
+  $('#myModal').on('hide.bs.modal', function () {
+      $('.modal-body iframe').attr('src', '');
+    });
+
+  // document ready
+});
