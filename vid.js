2b25143aeeee264ec2a7305cbd9123c4eeff6d8e
Navya32 <navyagupta32@gmail.com>
2021-10-03 10:53:37 +0530

    background blacked after video plays

--- vid.js
+++ vid.js
@@ -1,19 +1,23 @@
 // MODAL
-
 $(document).ready(function () {
   // Gets the video src from the data-src on each button
-
   var $videoSrc;
   $('.video-btn').click(function () {
     $videoSrc = $(this).data('src');
   });
 
+
   // when the modal is opened autoplay it
   $('#myModal').on('shown.bs.modal', function (e) {
-
+    //hiding elements
+    document.querySelector('video').style.visibility = 'hidden';
+    document.querySelector('.text').style.visibility = 'hidden';
+    document.querySelector('header').style.visibility = 'hidden';
+    document.querySelector('.video-btn').style.visibility = 'hidden';
     // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
     $('#video').attr('src', $videoSrc + '?autoplay=1&amp;modestbranding=1&amp;showinfo=0');
   });
+
 
   // stop playing the youtube video when I close the modal
   // $('#myModal').on('hide.bs.modal', function (e) {
@@ -23,6 +27,11 @@ $(document).ready(function () {
   // });
 
   $('#myModal').on('hide.bs.modal', function () {
+    //bringing the elements back
+      document.querySelector('video').style.visibility = 'visible';
+      document.querySelector('.text').style.visibility = 'visible';
+      document.querySelector('header').style.visibility = 'visible';
+      document.querySelector('.video-btn').style.visibility = 'visible';
       $('.modal-body iframe').attr('src', '');
     });
 
